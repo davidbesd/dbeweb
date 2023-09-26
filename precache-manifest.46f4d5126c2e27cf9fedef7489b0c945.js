8196d7dec6e72aa3722398941c80ef0903d65a6d
davidbesd <davidbesd@gmail.com>
2023-09-26 13:45:33 +0200

    Primer commit

---
+++ precache-manifest.46f4d5126c2e27cf9fedef7489b0c945.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07437977b178edc6db28",
+    "url": "/dbeweb/css/app.2b271a86.css"
+  },
+  {
+    "revision": "a473610f28bb70cd8f81ac5e9930356c",
+    "url": "/dbeweb/img/LogoDbeWeb.a473610f.png"
+  },
+  {
+    "revision": "c6f9cdb86d58ea8c46d6f2f4dc5d0ce5",
+    "url": "/dbeweb/index.html"
+  },
+  {
+    "revision": "24ace7724689b3270b6d",
+    "url": "/dbeweb/js/about.4226a755.js"
+  },
+  {
+    "revision": "07437977b178edc6db28",
+    "url": "/dbeweb/js/app.77987281.js"
+  },
+  {
+    "revision": "8feafd29f87dae148f17",
+    "url": "/dbeweb/js/chunk-vendors.7fb5d843.js"
+  },
+  {
+    "revision": "4d6374520c4c275d19e8d4fbd2e18667",
+    "url": "/dbeweb/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dbeweb/robots.txt"
+  }
+]);
